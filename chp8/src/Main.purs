122a08198884e874a91bc0c2fa6976d328e28ee3
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-10-19 21:57:38 +1100

    8.5

--- chp8/src/Main.purs
+++ chp8/src/Main.purs
@@ -4,6 +4,17 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Control.Plus (empty)
+import Data.Array ((..))
+
+countThrows :: Int -> Array (Array Int)
+countThrows n = do
+  x <- 1 .. 6
+  y <- 1 .. 6
+  if x + y == n
+    then pure [x, y]
+    else empty
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
