87dabe159b4ab59931084f58d4d4b26259c14d50
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-10-20 19:45:10 +1100

    Exc 8.7.6

--- chp8/src/Data/AddressBook/Validation.purs
+++ chp8/src/Data/AddressBook/Validation.purs
@@ -11,20 +11,23 @@ import Data.String.Regex.Flags (noFlags)
 import Data.Traversable (traverse)
 import Data.Validation.Semigroup (V, unV, invalid)
 import Partial.Unsafe (unsafePartial)
+import Data.Maybe(Maybe(..))
+import Data.Array(head, tail, sort, nub, foldM)
+import Data.List
 
 type Errors = Array String
 
 nonEmpty :: String -> String -> V Errors Unit
 nonEmpty field "" = invalid ["Field '" <> field <> "' cannot be empty"]
-nonEmpty _     _  = pure unit
+nonEmpty _ _ = pure unit
 
 arrayNonEmpty :: forall a. String -> Array a -> V Errors Unit
 arrayNonEmpty field [] = invalid ["Field '" <> field <> "' must contain at least one value"]
-arrayNonEmpty _     _  = pure unit
+arrayNonEmpty _ _ = pure unit
 
 lengthIs :: String -> Int -> String -> V Errors Unit
 lengthIs field len value | length value /= len = invalid ["Field '" <> field <> "' must have length " <> show len]
-lengthIs _     _   _     = pure unit
+lengthIs _ _ _ = pure unit
 
 phoneNumberRegex :: Regex
 phoneNumberRegex =
@@ -33,8 +36,8 @@ phoneNumberRegex =
       Right r -> r
 
 matches :: String -> Regex -> String -> V Errors Unit
-matches _     regex value | test regex value = pure unit
-matches field _     _     = invalid ["Field '" <> field <> "' did not match the required format"]
+matches _ regex value | test regex value = pure unit
+matches field _ _ = invalid ["Field '" <> field <> "' did not match the required format"]
 
 validateAddress :: Address -> V Errors Address
 validateAddress (Address o) =
@@ -56,3 +59,52 @@ validatePerson (Person o) =
 
 validatePerson' :: Person -> Either Errors Person
 validatePerson' p = unV Left Right $ validatePerson p
+
+third :: forall a. Array a -> Maybe a
+third xs = do
+  ys <- tail xs
+  zs <- tail ys
+  z  <- head zs
+  pure z
+
+sums :: Array Int -> Array Int
+sums xs = sort $ nub $ foldM (\a b -> [a, b, a + b]) 0 xs
+
+-- ap :: forall m a b. Monad m => m (a -> b) -> m a -> m b
+-- apply :: forall a b. f (a -> b) -> f a -> f b
+-- ap is equal to apply where apply 'f' is specialized to Monad
+
+filterM :: forall m a. Monad m => (a -> m Boolean) -> List a -> m (List a)
+filterM _ Nil = pure Nil
+filterM f (x : xs) = do
+  x' <- f x
+  xs' <- filterM f xs
+  pure (if x' then x : xs' else xs')
+
+-- Prove lift2 f (pure a) (pure b) is equal to pure (f a b)
+--
+-- lift2 :: forall f a b c. Applicative f => (a -> b -> c) -> f a -> f b -> f c
+-- lift2 f a b = f <$> a <*> b
+--
+-- So, lift2 f (pure a) (pure b) is equal to:
+--   f <$> (pure a) <*> (pure b)
+--
+-- Given: <$> (map) f a = do
+--          x <- a
+--          pure (f x)
+--
+-- f <$> (pure a) <*> (pure b) is equal to:
+--   pure (f a) <*> (pure b)
+--   where types are: m (b -> c) <*> m b
+--
+-- Those types match our <*> function:
+-- <*> (ap) is equal to:
+--   ap :: forall m a b. Monad m => m (a -> b) -> m a -> m b
+--   ap mf ma = do
+--     f <- mf
+--     a <- ma
+--     pure (f a)
+--
+-- Therefore pure (f a) <*> (pure b) is equal to:
+--   pure (f a b)
+-- So, lift2 f (pure a) (pure b) is equal to pure (f a b)
