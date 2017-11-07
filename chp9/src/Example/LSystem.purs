0714455c205fd70ece4546a82dcc205e3ff41552
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-11-07 16:59:21 +1100

    Exc 9.9.7

--- chp9/src/Example/LSystem.purs
+++ chp9/src/Example/LSystem.purs
@@ -11,6 +11,7 @@ import Graphics.Canvas (CANVAS, closePath, fillPath, getCanvasElementById,
                         setShadowOffsetY, setStrokeStyle, strokePath)
 import Math as Math
 import Partial.Unsafe (unsafePartial)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
 lsystem :: forall a m s. Monad m =>
                          Array a ->
@@ -35,10 +36,9 @@ type State =
   , theta :: Number
   }
 
-main :: Eff (canvas :: CANVAS) Unit
 main = void $ unsafePartial do
-  Just canvas <- getCanvasElementById "canvas"
-  ctx <- getContext2D canvas
+  -- Just canvas <- getCanvasElementById "canvas"
+  -- ctx <- getContext2D canvas
 
   let
     initial :: Sentence
@@ -50,26 +50,41 @@ main = void $ unsafePartial do
     productions (F false) = [(F false), L, (F true), L, (F false), R, (F true), R, (F false), R, (F true), R, (F false), L, (F true), L, (F false)]
     productions (F true) = [(F true), R, (F false), R, (F true), L, (F false), L, (F true), L, (F false), L, (F true), R, (F false), R, (F true)]
 
-    interpret :: State -> Alphabet -> Eff (canvas :: CANVAS) State
-    interpret state L = pure $ state { theta = state.theta - Math.pi / 3.0 }
-    interpret state R = pure $ state { theta = state.theta + Math.pi / 3.0 }
-    interpret state (F _) = do
-      let x = state.x + Math.cos state.theta * 1.5
-          y = state.y + Math.sin state.theta * 1.5
-      _ <- lineTo ctx x y
-      pure { x, y, theta: state.theta }
+    -- interpret :: State -> Alphabet -> Eff (canvas :: CANVAS) State
+    -- interpret state L     = pure $ state { theta = state.theta - Math.pi / 3.0 }
+    -- interpret state R     = pure $ state { theta = state.theta + Math.pi / 3.0 }
+    -- interpret state (F _) = do
+    --   let x = state.x + Math.cos state.theta * 1.5
+    --       y = state.y + Math.sin state.theta * 1.5
+    --   _ <- lineTo ctx x y
+    --   pure { x, y, theta: state.theta }
+
+    interpretTrace :: State -> Alphabet -> Eff (console :: CONSOLE) State 
+    interpretTrace state L         = do
+                                     _ <- log $ show "L"
+                                     pure $ state
+    interpretTrace state R         = do
+                                     _ <- log $ show "R"
+                                     pure $ state
+    interpretTrace state (F true)  = do
+                                     _ <- log $ "(F t)"
+                                     pure $ state
+    interpretTrace state (F false) = do
+                                     _ <- log $ "(F f)"
+                                     pure $ state
 
     initialState :: State
     initialState = { x: 120.0, y: 200.0, theta: 0.0 }
 
-  _ <- moveTo ctx initialState.x initialState.y
+  -- _ <- moveTo ctx initialState.x initialState.y
 
-  _ <- setFillStyle "#ABE188" ctx
-  _ <- setShadowOffsetX 10.0 ctx
-  _ <- setShadowOffsetY 10.0 ctx
-  _ <- setShadowBlur 3.0 ctx
-  _ <- setShadowColor "#777" ctx
+  -- _ <- setFillStyle "#ABE188" ctx
+  -- _ <- setShadowOffsetX 10.0 ctx
+  -- _ <- setShadowOffsetY 10.0 ctx
+  -- _ <- setShadowBlur 3.0 ctx
+  -- _ <- setShadowColor "#777" ctx
 
-  fillPath ctx $ do 
-    _ <- lsystem initial productions interpret 4 initialState
-    closePath ctx
+  -- fillPath ctx $ do 
+  --   _ <- lsystem initial productions interpret 4 initialState
+  --   closePath ctx
+  lsystem initial productions interpretTrace 1 initialState
