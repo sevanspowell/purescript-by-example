849822785bc377920fbacaabfb2d8d1f233f69d8
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-12-02 18:48:56 +1100

    12.6.3

--- chp12/src/Exercises.purs
+++ chp12/src/Exercises.purs
@@ -1,17 +1,22 @@
 module Exercises where
 
 import Files
+import Network.HTTP.Client
 import Prelude
 
 import Control.Monad.Cont.Trans (ContT(..), runContT)
 import Control.Monad.Eff (kind Effect, Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Control.Monad.Except.Trans (ExceptT(..))
+import Control.Monad.Except.Trans (ExceptT(..), runExceptT)
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
 import Data.Function.Uncurried (Fn2, runFn2)
 import Data.Traversable (traverse, traverseDefault, traverse_)
 import Types (Async)
+
+--
+-- Concat Files
+--
 
 concatFilesCont
   :: forall eff
@@ -48,6 +53,10 @@ concatFilesManyContEx xs dest = do
   concat <- pure $ foldl (<>) "" collect
   writeFileContEx dest concat
 
+--
+-- Timeout
+--
+
 type Milliseconds = Int
 
 foreign import data TIMEOUT :: Effect
@@ -71,3 +80,16 @@ setTimeoutUsage = do
   _ <- runContT (setTimeoutCont 5000) (\x -> pure $ unit)
   logShow "hi"
 
+--
+-- HTTP Client
+--
+
+-- runContT (get "https://jsonplaceholder.typicode.com/posts/1") (logShow)
+
+getEx :: forall eff. URI -> ExceptT ErrorCode (Async (http :: HTTP | eff)) String
+getEx uri = ExceptT $ get uri
+
+getToFile :: forall eff. URI -> String -> ExceptT ErrorCode (Async (http :: HTTP, fs :: FS | eff)) Unit
+getToFile uri path = do
+  response <- getEx uri
+  writeFileContEx path response
